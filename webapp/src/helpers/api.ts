3280b9109e101f106334ca01e393ee1042d615cd
Ariel Aguirre <alaguirre@fi.uba.ar>
2025-07-21 22:34:19 -0300

    chore: add webapp env var

--- webapp/src/helpers/api.ts
+++ webapp/src/helpers/api.ts
@@ -1,6 +1,4 @@
-import { Transaction } from "@/types/banking";
-
-const url = "http://localhost:3000/api";
+const url = import.meta.env.VITE_API_URL;
 
 export const getTransactions = async (userId: string) => {
   try {
